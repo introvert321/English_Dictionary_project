c3ae25c61bc3833eb1ef184a62a64637e6de32d3
Deepanshu Saini <sainideepanshu2509@gmail.com>
2021-07-23 13:23:59 +0530

    Added conditions inside the function

--- app.py
+++ app.py
@@ -4,7 +4,16 @@ from difflib import get_close_matches
 data = json.load(open("data.json"))
 
 def dictionary(word):
-    return word[data]
+    word = word.lower()
+    if word in data: 
+        return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        yn = input("Did you mean {0} instead of {1}. If yes enter 'y'".format(get_close_matches(word, data.keys())[0], word))
+        yn = yn.lower()
+        if yn == "y": return data[get_close_matches(word, data.keys())[0]]
+        else: return "Sorry we couldn't find what you want.."
+    else:
+        return "This word doesn't exist: "
 
 word = input("Enter the word: ")
 print(dictionary(word))
