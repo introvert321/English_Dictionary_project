6ce8486a03860b3d836ae0699d84300c844edbed
Deepanshu Saini <sainideepanshu2509@gmail.com>
2021-07-23 13:22:55 +0530

    Load our json data and initialize the function

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
 import json
 from difflib import get_close_matches
+
+data = json.load(open("data.json"))
+
+def dictionary(word):
+    return word[data]
+
+word = input("Enter the word: ")
+print(dictionary(word))
